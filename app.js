5f54d49b5a73a822db458a0726730dd494069c7a
truongthanhvy <thanhvy1710.truong@gmail.com>
2021-07-09 23:41:14 +0700

    init commit

---
+++ app.js
@@ -0,0 +1,22 @@
+//cau 1
+// let a="program";
+// let b="";
+// for(let i=a.length-1;i<=0;i--){
+//     b=b+a[i];
+//     console.log(b);
+// }
+
+// cau 2
+// const chuoi= 'flavio'
+// const inhoachuoi= chuoi.charAt(0).toUpperCase() +chuoi.slice(1)
+// console.log(inhoachuoi);
+//câu 3
+let mang=["chim","chim","chó",12,"mèo",12,13,13];
+console.log(mang);
+for(let i=0;i<=mang.length-1;i++){
+    console.log(mang[i]);
+    if(mang[i]=="chó"|| mang[i]=="chim"||mang[i]==12||mang[13]){
+        
+
+    }
+}
